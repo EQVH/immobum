b70756196a8b95236ed8c8261bf6823d1b89dabf
Isaac <isaac@Isaacs-MacBook-Air.local>
2026-06-27 13:53:15 -0600

    added ci/cd

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "immobum.com",
+  site: "https://immobum.com",
   vite: {
     plugins: [tailwindcss()],
   },
